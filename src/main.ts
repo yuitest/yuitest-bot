b35ecdc2bf8be05baa4c790ba100c07a2f8f68e0
yuitest <yuitest@cjhat.net>
2023-07-11 23:32:36 +0900

    Hello, World を投稿する

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,25 @@
-import { hello } from '.'
-console.log(hello())
+import Twitter from 'twitter'
+
+function getClientFromEnv() {
+  return new Twitter({
+    consumer_key: process.env.CONSUMER_KEY || '',
+    consumer_secret: process.env.CONSUMER_SECRET || '',
+    access_token_key: process.env.ACCESS_TOKEN_KEY || '',
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET || '',
+  })
+}
+
+function tweet(text: string) {
+  const client = getClientFromEnv()
+  client.post(
+    'statuses/update',
+    { status: text },
+    (error, _tweet, _response) => {
+      if (error) throw error
+    }
+  )
+}
+
+if (require.main === module) {
+  tweet('Hello, world!')
+}
