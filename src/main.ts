12aa58c4db9fdaf2617f828cc1bf5231f58f98b0
yuitest <yuitest@cjhat.net>
2023-07-11 23:39:58 +0900

    ランダムツイートを実装

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import fs from 'fs'
+
 import Twitter from 'twitter'
 
 function getClientFromEnv() {
@@ -7,6 +9,12 @@ function getClientFromEnv() {
     access_token_key: process.env.ACCESS_TOKEN_KEY || '',
     access_token_secret: process.env.ACCESS_TOKEN_SECRET || '',
   })
+}
+
+function buildCorpus(): string[] {
+  const text = fs.readFileSync('./corpus.txt', 'utf-8')
+  const lines = text.split('\n')
+  return lines.map((line) => line.replace('\\r\\n', '\r\n'))
 }
 
 function tweet(text: string) {
@@ -20,6 +28,17 @@ function tweet(text: string) {
   )
 }
 
+function pickRandom<T>(array: T[]): T {
+  const index = Math.floor(Math.random() * array.length)
+  return array[index]
+}
+
+function randomTweet() {
+  const corpus = buildCorpus()
+  const text = pickRandom(corpus)
+  tweet(text)
+}
+
 if (require.main === module) {
-  tweet('Hello, world!')
+  randomTweet()
 }
