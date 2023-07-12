cf6d265e94edcc712bbfb8a9db26498820e3e6f2
yuitest <yuitest@cjhat.net>
2023-07-12 21:43:56 +0900

    メッセージの書式を関数にしてテストを追加

--- src/habit.ts
+++ src/habit.ts
@@ -1,7 +1,27 @@
 import { DateTime } from 'luxon'
 
-import { getTodaysJournal, summarizeJournal } from './habitify'
+import { getTodaysJournal, summarizeJournal, Summary } from './habitify'
 import { tweet } from './twitter'
+
+export function buildMessage(
+  summary: Summary,
+  targetDate: string,
+  targetTime: string
+): string {
+  const ratio = (summary.completedTasks / summary.allOfTasks) * 100
+  let message = `#ゆいてすと日課 現在 (${targetDate} ${targetTime}) の日課の達成状況
+(完了 / 全タスク) = (${summary.completedTasks} / ${
+    summary.allOfTasks
+  }) = ${ratio.toFixed(1)}%`
+
+  if (summary.failedTasks > 0) {
+    message += `\n失敗: ${summary.failedTasks} 件`
+  }
+  if (summary.skippedTasks > 0) {
+    message += `\nスキップ: ${summary.skippedTasks} 件`
+  }
+  return message
+}
 
 if (require.main === module) {
   ;(async () => {
@@ -12,18 +32,7 @@ if (require.main === module) {
     const queryDate = `${targetDate}T${targetTime}+09:00`
     const data = await getTodaysJournal(apiKey, queryDate)
     const summary = summarizeJournal(data)
-    const ratio = (summary.completedTasks / summary.allOfTasks) * 100
-    let message = `#ゆいてすと日課 現在 (${targetDate} ${targetTime}) の日課の達成状況
-(完了 / 全タスク) = (${summary.completedTasks} / ${
-      summary.allOfTasks
-    }) = ${ratio.toFixed(1)}%`
-
-    if (summary.failedTasks > 0) {
-      message += `\n失敗: ${summary.failedTasks}件`
-    }
-    if (summary.skippedTasks > 0) {
-      message += `\nスキップ: ${summary.skippedTasks}件`
-    }
+    const message = buildMessage(summary, targetDate, targetTime)
     tweet(message)
   })()
 }
