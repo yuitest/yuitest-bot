a663eebf8fb7ae6d38f2e43a9c014f1e53875064
yuitest <yuitest@cjhat.net>
2023-08-13 13:14:30 +0900

    バージョンを上げたらツイートする

---
+++ src/update.ts
@@ -0,0 +1,10 @@
+import fs from 'fs'
+
+import { tweet } from './twitter'
+
+if (require.main === module) {
+  ;(async () => {
+    const hash = fs.readFileSync('.git/refs/heads/main', 'utf8')
+    await tweet(`じゃきーん！バージョン ${hash} になりました`)
+  })()
+}
